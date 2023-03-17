9bdc2e16d0463c63f24fccff44cc7e0bb14bda56
DiskitAMO <diskit.angmo@catalyst.sh>
2023-03-17 18:57:24 +0530

    file import changed in home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Home.scss";
-import Featured from "../../../components/featured/featured";
+import Featured from "../../../components/featured/Featured";
 import Slide from "../../../components/Slide/Slide";
 import TrustedBy from "../../../components/trustedBy/TrustedBy";
 import { cards, projects } from "../../data";
